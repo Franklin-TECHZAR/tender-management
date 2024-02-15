b80dadd6f14b38c4bc4246703b637da5e58b481c
suvetha <suvethajeeva37@gmail.com>
2024-02-15 15:16:19 +0530

    migration changes

--- database/migrations/2024_02_15_151112_create_purchase_table.php
+++ database/migrations/2024_02_15_151112_create_purchase_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('purchases', function (Blueprint $table) {
+        Schema::create('purchase', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('job_order_id')->nullable();
             $table->string('invoice_no')->nullable();
@@ -19,16 +19,16 @@ return new class extends Migration
             $table->date('date');
             $table->unsignedBigInteger('vendor_id')->nullable();
             $table->unsignedBigInteger('material_id')->nullable();
-            $table->int('quantity');
+            $table->integer('quantity');
             $table->string('unit');
             $table->decimal('amount', 10, 2);
             $table->decimal('gst', 10, 2);
             $table->decimal('total', 10, 2);
+
             $table->timestamps();
             $table->dateTime('deleted_at')->nullable();
 
-
-            $table->foreign('job_order_id')->references('id')->on('job_orders')->onDelete('set null');
+            $table->foreign('job_order_id')->references('id')->on('tenders')->onDelete('set null');
             $table->foreign('vendor_id')->references('id')->on('vendors')->onDelete('set null');
             $table->foreign('material_id')->references('id')->on('materials')->onDelete('set null');
         });
@@ -39,6 +39,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('purchases');
+        Schema::dropIfExists('purchase');
     }
 };
